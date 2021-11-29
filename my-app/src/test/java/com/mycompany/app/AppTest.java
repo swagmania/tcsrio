1086a27d7d733e116472f36586db6c6fadaceb9d
srinu.pj <56768344+swagmania@users.noreply.github.com>
2021-11-29 18:07:06 +0530

    Update AppTest.java

--- my-app/src/test/java/com/mycompany/app/AppTest.java
+++ my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -1,20 +1,13 @@
-package com.mycompany.app;
-
-import static org.junit.Assert.assertTrue;
-
+package demo.tests;
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class JUnitProgram {
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void test_JUnit() {
+        System.out.println("This is the testcase in this class");
+        String str1="This is the testcase in this class";
+        assertEquals("This is the testcase in this class", str1);
     }
 }
