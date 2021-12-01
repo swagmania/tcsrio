cbb18b68ad23d467e47a3db53d822fd6e7b0771c
srinu.pj <56768344+swagmania@users.noreply.github.com>
2021-12-01 20:27:52 +0530

    Update AppTest.java

--- my-app/src/test/java/com/mycompany/app/AppTest.java
+++ my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -1,13 +1,48 @@
-package demo.tests;
-import static org.junit.Assert.*;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-public class JUnitProgram {
-    @Test
-    public void test_JUnit() {
-        System.out.println("This is the testcase in this class");
-        String str1="This is the testcase in this class";
-        assertEquals("This is the testcase in this class", str1);
+package com.techprimers.testing;
+
+import org.junit.jupiter.api.*;
+
+class FizzBuzzTest {
+
+    public FizzBuzz fB;
+
+    @BeforeEach
+    public void setUp() {
+        fB = new FizzBuzz();
     }
+
+    @DisplayName("Play FizzBuzz with number = 1")
+    @Test
+    public void testNumber() {
+        String fizzBuzz = fB.play(1);
+        Assertions.assertEquals(fizzBuzz, "1");
+    }
+
+    @DisplayName("Play FizzBuzz with number = 3")
+    @Test
+    public void testFizz() {
+        String fizzBuzz = fB.play(3);
+        Assertions.assertEquals(fizzBuzz, "Fizz");
+    }
+
+    @DisplayName("Play FizzBuzz with number = 5")
+    @Test
+    public void testBuzz() {
+        String fizzBuzz = fB.play(5);
+        Assertions.assertEquals(fizzBuzz, "Buzz");
+    }
+
+    @DisplayName("Don't Play FizzBuzz with number = 0")
+    @Test
+    public void testZero() {
+
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> fB.play(0));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        fB = null;
+    }
+
 }
